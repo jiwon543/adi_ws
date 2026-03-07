feec6153eb72c63e775856bcdec6d364994dd03d
jiwon543 <won2b834@gmail.com>
2026-03-07 17:29:54 +0900

    Delete build and devel

--- build/main/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/adi_ws/src/main
-# Build directory: /root/adi_ws/build/main
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
